5c17f32a5080f3cfa92f5ebb34a6e452d37479fc
sdairs <sdairs@users.noreply.github.com>
2025-01-22 13:31:53 +0000

    token

--- apps/web/src/components/tools/vercel/deployment-duration-ts.tsx
+++ apps/web/src/components/tools/vercel/deployment-duration-ts.tsx
@@ -1,16 +1,18 @@
 'use client'
 
 import { LineChart } from '@tinybirdco/charts'
+import { useQueryState } from 'nuqs'
 
 export function VercelDeploymentDuration(params: {
   date_from?: string
   date_to?: string
   time_range?: string
 }) {
+  const [token] = useQueryState('token')
   return (
     <LineChart
       endpoint="https://api.tinybird.co/v0/pipes/vercel_deployment_duration.json"
-      token = "p.eyJ1IjogIjdjNjJiZGRjLWQ3MDItNDBiMy04YTc5LTdkYWQ4ODZmN2FhYiIsICJpZCI6ICIwYWYxMDYzMi1iMTdhLTQzMWEtYjlkOS0wZjdlZWU1M2I3ODMiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.EUq1kGyKew75WAzACmSmPLjfjDI0F5Jj0DXQAQSqhD8"
+      token={token ?? ''}
       index="period"
       categories={['avg_duration', 'p95_duration']}
       colorPalette={['#27F795', '#008060', '#0EB1B9', '#9263AF', '#5A6FC0', '#86BFDB', '#FFC145', '#FF6B6C', '#DC82C8', '#FFC0F1']}
--- apps/web/src/components/tools/vercel/git-analytics.tsx
+++ apps/web/src/components/tools/vercel/git-analytics.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import { BarList } from '@tinybirdco/charts'
+import { useQueryState } from 'nuqs'
 
 export function GitAnalytics(params: {
   date_from?: string
   date_to?: string
 }) {
+  const [token] = useQueryState('token')
   return (
     <BarList
       endpoint="https://api.tinybird.co/v0/pipes/vercel_git_analytics.json"
-      token = "p.eyJ1IjogIjdjNjJiZGRjLWQ3MDItNDBiMy04YTc5LTdkYWQ4ODZmN2FhYiIsICJpZCI6ICIwYWYxMDYzMi1iMTdhLTQzMWEtYjlkOS0wZjdlZWU1M2I3ODMiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.EUq1kGyKew75WAzACmSmPLjfjDI0F5Jj0DXQAQSqhD8"
+      token={token ?? ''}
       index="author"
       categories={['commits']}
       colorPalette={['#27F795', '#008060', '#0EB1B9', '#9263AF', '#5A6FC0', '#86BFDB', '#FFC145', '#FF6B6C', '#DC82C8', '#FFC0F1']}
